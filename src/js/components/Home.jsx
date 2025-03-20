9e04627190407adfb286ed2f9d227705a1f5e171
sergiorjuel <110314668+sergiorjuel@users.noreply.github.com>
2025-03-20 03:22:42 +0000

    Second commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -5,44 +5,56 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [ inputValue, setInputValue ] = useState("");
+	const [inputValue, setInputValue] = useState("");
 	const [todos, setTodos] = useState([]);
-	
+
 	return (
 		<div className="titulo">
 			<h1>todos</h1>
-		<div className="container">
-			
-			<ul>
-				<li>
-					<input type="text" 
-					onChange={(e) => setInputValue(e.target.value)}
-					value={inputValue}
-					onKeyDown={(e) => e.key === "Enter" ? setTodos(todos.concat(inputValue)) : null}
-					placeholder="What do you need to do?"/>
-				
-				</li>
-				{todos.map((item, index) => (
-					<li key={index}>
-						{item}<i class="fa-solid fa-trash"
-						 onClick={() => 
-							setTodos(
-								todos.filter(
-									(t, currentIndex) => 
-										index !== currentIndex)
-									)
-									}></i>
+			<div className="container">
+
+				<ul>
+					<li>
+						<input
+							type="text"
+							onChange={(e) => setInputValue(e.target.value)}
+							value={inputValue}
+							onKeyDown={(e) => {
+								if (e.key === "Enter") {
+									setTodos(todos.concat(inputValue));
+									setInputValue("");
+								}
+							}}
+							placeholder="What do you need to do?"
+						/>
 					</li>
-				))}
+					{todos.map((item, index) => (
+						<li key={index}>
+							{item} 
+							<button 
+								onClick={() =>
+									setTodos(
+										todos.filter(
+											(t, currentIndex) =>
+												index !== currentIndex)
+									)
+								}
+							>
+								<i className="fa-solid fa-trash"></i>
+							
+							</button>
+						</li>
+					))}
 
-			</ul>
-		
-			<div className="todos-counter">
-			{ `${todos.length} items left `}
+				</ul>
+
+				<div className="todos-counter">
+					{todos.length === 0 ? "No hay tareas, agrega una!" : `${todos.length} items left`}
+
+				</div>
+
+
 			</div>
-		
-
-		</div>
 		</div>
 	);
 };
